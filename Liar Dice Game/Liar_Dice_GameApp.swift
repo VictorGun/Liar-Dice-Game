41978bf179c2ac483e04e4ce542d12bc9726e259
VictorGun <Thunderboomboom7@gmail.com>
2022-10-28 09:48:44 -0500

    Initial Commit

---
+++ Liar Dice Game/Liar_Dice_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Liar_Dice_GameApp.swift
+//  Liar Dice Game
+//
+//  Created by Victor Gunderson on 10/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Liar_Dice_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
