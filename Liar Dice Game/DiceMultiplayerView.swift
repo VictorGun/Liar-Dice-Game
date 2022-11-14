e6378e0da847cc67d13dd768f6ae8bd9fb8d87f6
VictorGun <Thunderboomboom7@gmail.com>
2022-11-14 17:32:23 -0600

    Started 2 player game. Interface about 70% done.

--- Liar Dice Game/DiceMultiplayerView.swift
+++ Liar Dice Game/DiceMultiplayerView.swift
@@ -6,10 +6,71 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct DiceMultiplayerView: View {
+    @State private var total = 0
+    @State private var player: AVAudioPlayer!
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Spacer()
+                HStack {
+                    DiceView(score: $total)
+                    Spacer()
+                    DiceView(score: $total)
+                }
+                .padding()
+                
+                // winning or losing
+                if total >= 20 {
+                    Text("You Won!")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding()
+                    NavigationLink(destination: ContentView()) {
+                        Label("Back To Main Menu", systemImage: "arrowtriangle.right.fill")
+                            .font(Font.custom("impact", size: 20))
+                    }
+                } else if total <= -15  {
+                    Text("You Lose!")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding()
+                    NavigationLink(destination: ContentView()) {
+                        Label("Back To Main Menu", systemImage: "arrowtriangle.right.fill")
+                            .font(Font.custom("impact", size: 20))
+                    }
+                }
+                Spacer()
+                HStack {
+                    Text("PLAYER 1")
+                        .font(Font.custom("impact", size: 20))
+                    Spacer()
+                        .frame(width: 134)
+                    Text("PLAYER 2")
+                        .font(Font.custom("impact", size: 20))
+                        .padding()
+                }
+                HStack {
+                    Text("\(total)")
+                        .font(Font.custom("impact", size: 20))
+                        Spacer()
+                        .frame(width: 225)
+                    Text("\(total)")
+                        .font(Font.custom("impact", size: 20))
+                }
+            }
+            if total >= 2 {
+                Text("You Won!")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                NavigationLink(destination: ContentView()) {
+                    Label("Back To Main Menu", systemImage: "arrowtriangle.right.fill")
+                        .font(Font.custom("impact", size: 20))
+                }
+            }
+        }
     }
 }
 
