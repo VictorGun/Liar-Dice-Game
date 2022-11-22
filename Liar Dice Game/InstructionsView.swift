16d0238c69ba5ce12c35abc4675a4a0691015582
VictorGun <Thunderboomboom7@gmail.com>
2022-11-22 09:48:16 -0600

    finished instructions, also added the font from last project simon

--- Liar Dice Game/InstructionsView.swift
+++ Liar Dice Game/InstructionsView.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct InstructionsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            Text("HOW TO PLAY/RULES")
+                .font(Font.custom("impact", size: 50))
+            Text("""
+• Roll the dice to try to get a even or odd number
+
+• If the score is equal to 25, then you win!
+
+• If the score is equal to -15, then you lose :(
+
+• If the dice lands on a even pip it will give you points, if the dice lands on a odd pip it will take away points.
+
+(NUMBERS ON THE PIPS ARE DIVDED BY 2 SO THE GAME ISNT AS EASY... Ex: IF THE DICE LANDS ON 6 PIPS, THEN YOU GET 3 POINTS... 6/2 = 3)
+
+
+• In multiplayer, choose your own dice (EITHER P1 or P2) and start rolling. The other player shouldnt be able to roll the dice when the player is going.
+
+Multiplayer win/lose/score is the same as single player
+
+""") .font(Font.custom("impact", size: 18))
+                .padding(5)
+            Spacer()
+        }
     }
 }
 
